e5590f4ce6094e8dd4acc26e3ffabf57a3337129
mani2002 <manikantavinjamuri8522@gmail.com>
2024-08-02 12:24:25 +0530

    'added_vercel_links_to_cors'

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://crm10.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -41,7 +41,7 @@ connection.connect((err) => {
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://crm10.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true,
   })
